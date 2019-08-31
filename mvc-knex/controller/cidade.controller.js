771145d682339b3b7e1102d3db5a1a6b8a07b640
FelipeHannemann <hannemannfelipe@outlook.com>
2019-08-30 21:53:23 -0300

    Comit 30/08

--- mvc-knex/controller/cidade.controller.js
+++ mvc-knex/controller/cidade.controller.js
@@ -1,4 +1,4 @@
-const repository = require('../repository/estado.repository');
+const repository = require('../repository/cidade.repository');
 const estado = [];
 
 module.exports = {
@@ -6,7 +6,25 @@ module.exports = {
     find: (req, res) => {
 
         repository.find().then(result => {
-            res.send(result);
+
+            const cidades = [];
+            //converte de RELACIONAL para OBJETO
+            for (item of result) {
+
+                let cidade = {
+                    id: item.cidade_id,
+                    nome: item.cidade_nome,
+                    estado: {
+                        id: item.estado_id,
+                        nome: item.estado_nome,
+                        sigra: item.estado_sigla
+                    }
+
+                }
+                cidades.push(cidade);
+            }
+            console.log(result);
+            res.send(cidades);
         }).catch(error => {
             res.status(500).send(error);
         });
@@ -26,9 +44,15 @@ module.exports = {
     },
 
     create: (req, res) => {
-        repository.create(req.body).then(result => {
-            req.body.id = result[0];
-            res.send(req.body);
+        //Converter de OBJETO para RELACIONAL
+        const cidade = {
+            nome: req.body.nome,
+            estado_id: req.body.estado.id
+        }
+        repository.create(cidade).then(result => {
+            console.log(cidade);
+      //      req.body.id = result[0];
+            res.send(cidade);
         }).catch(error => {
             res.status(500).send(error)
         });
@@ -37,6 +61,7 @@ module.exports = {
 
     update: (req, res) => {
         //Atualiza o id do objeto do req.body
+
         repository.update(req.body).then(result => {
             if (result.affectedRows == 0) {
                 res.status(404).send('not found');
@@ -49,15 +74,9 @@ module.exports = {
         });
 
     },
-    delete: (req,res) => {
-        repository.delete(req.params).then(result => {
-
-            if (!result[0]) {
-                res.status(404).send('not found');
-            } else {
-                res.send(result[0]);
-            }
-
+    delete: (req, res) => {
+        repository.delete().then(result => {
+            res.status(204).send();
         }).catch(error => {
             res.status(500).send(error);
         });
