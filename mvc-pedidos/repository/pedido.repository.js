83ec353827e67604eee163d2e2a92070e6bb5300
FelipeHannemann <hannemannfelipe@outlook.com>
2019-09-03 22:20:24 -0300

    Comit Aula 03/09

---
+++ mvc-pedidos/repository/pedido.repository.js
@@ -0,0 +1,38 @@
+const connection = require('../mysql-connection');
+
+const query = 'SELECT P.ID AS ID_PED, P.CODIGO AS CODIGO_PED, P.DTPEDIDO AS DATA_PED, ' +
+              'P.OBSERVACAO AS OBSERVACAO_PED, C.ID AS ID_CLIENTE, C.CODIGO AS CODIGO_CLI, ' +
+              'C.NOME AS NOME_CLI, C.EMAIL AS EMAIL_CLI, V.ID AS ID_VENDEDOR, V.CODIGO AS CODIGO_VENDEDOR, '+
+              'V.NOME AS NOME_VENDEDOR, C.EMAIL AS EMAIL_VENDEDOR, PROD.ID AS ID_PROD,' +
+              'PROD.CODIGO AS CODIGO_PROD, PROD.NOME AS PROD_NOME, PROD.DESCRICAO AS PROD_DESCRICAO, '+
+              'PROD.PRECO AS PROD_PRECO '+
+              'FROM PEDIDO P '+ 
+              'JOIN CLIENTE C ON P.CLIENTE_ID = C.ID '+
+              'JOIN VENDEDOR V ON P.VENDEDOR_ID = V.ID '+
+              'JOIN PRODUTO PROD ON P.ID = PROD.ID ';
+
+module.exports = {
+
+    find: (callback) => {
+        connection.query(query, callback);
+},
+
+    findById: (params, callback) => {        
+        connection.query('SELECT * FROM vendedor WHERE ID = ?', [params.id], callback);
+},
+
+    create: (params, callback) => {
+            connection.query('INSERT INTO pedido (codigo,nome,email) VALUES(?,?,?)', [params.codigo,
+            params.nome, params.email], callback);           
+        
+    },
+
+    update: (params, callback) => {
+        connection.query('UPDATE vendedor SET codigo = ?, nome = ?, email = ? WHERE id = ?', [params.codigo,
+        params.nome, params.email, params.id], callback);           
+    
+},
+    delete: (params, callback) => {        
+        connection.query('DELETE  FROM vendedor WHERE ID = ?', [params.id], callback);
+}
+};
--- mvc-pedidos/repository/produto.repository.js
+++ mvc-pedidos/repository/produto.repository.js
@@ -11,14 +11,14 @@ module.exports = {
 },
 
     create: (params, callback) => {
-            connection.query('INSERT INTO produto (codigo,nome,descricao) VALUES(?,?,?)', [params.codigo,
-            params.nome, params.descricao], callback);           
+            connection.query('INSERT INTO produto (codigo,nome,descricao, preco) VALUES(?,?,?,?)', [params.codigo,
+            params.nome, params.descricao, params.preco], callback);           
         
     },
 
     update: (params, callback) => {
-        connection.query('UPDATE produto SET codigo = ?, nome = ?, descricao = ? WHERE id = ?', [params.codigo,
-        params.nome, params.descricao, params.id], callback);           
+        connection.query('UPDATE produto SET codigo = ?, nome = ?, descricao = ?, preco = ? WHERE id = ?', [params.codigo,
+        params.nome, params.descricao, params.preco, params.id], callback);           
     
 },
     delete: (params, callback) => {        
